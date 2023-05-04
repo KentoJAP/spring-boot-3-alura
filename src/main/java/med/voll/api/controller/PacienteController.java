913a90170382ceb38653b871988b16e2485820df
Juliana Andrade Pereira <juliana.andrade.p@hotmail.com>
2023-05-04 20:11:25 -0300

    Adicionei o @PageableDefault na listagem de pacientes

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -7,6 +7,7 @@ import med.voll.api.paciente.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -25,7 +26,7 @@ public class PacienteController {
     }
 
     @GetMapping
-    public Page<DadosListagemPaciente> listar(Pageable paginacao){
+    public Page<DadosListagemPaciente> listar(@PageableDefault(size= 10, sort={"nome"}) Pageable paginacao){
         return repository.findAll(paginacao).map(DadosListagemPaciente::new);
     }
 }
